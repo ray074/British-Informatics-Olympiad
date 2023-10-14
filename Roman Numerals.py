6a9d189d7ca77c4315ee20c5043bf34a0f08c810
ray074 <147860006+ray074@users.noreply.github.com>
2023-10-14 18:39:48 +0100

    Add files via upload

---
+++ Roman Numerals.py
@@ -0,0 +1,21 @@
+numerals = {
+    
+    1: 'I', 4: 'IV', 5: 'V', 9: 'IX',
+    10: 'X', 40: 'XL', 50: 'L', 90: 'XC',
+    100: 'C', 400: 'CD', 500: 'D', 900: 'CM',
+    1000: 'M'
+}
+
+nums = [num for num in sorted(numerals.keys(), reverse=True)]
+inp = int(input())
+
+romans = []
+
+while inp != 0:
+    for num in nums:
+        if num <= inp:
+            inp -= num
+            romans.append(numerals[num])
+            break
+
+print("".join(romans))
