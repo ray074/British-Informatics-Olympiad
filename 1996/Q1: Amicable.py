5c4a973be1861a57727479e16d7b1b66344acb7a
ray074 <147860006+ray074@users.noreply.github.com>
2023-10-15 20:28:22 +0100

    Update Q1: Amicable.py

--- 1996/Q1: Amicable.py
+++ 1996/Q1: Amicable.py
@@ -1,8 +1,9 @@
 def sum_factors(num):
-    return sum([factor for factor in range(1, num-1) if num % factor == 0])
+    return sum([factor for factor in range(1, num) if num % factor == 0])
 
 def main():
     num1, num2 = int(input()), int(input())
-    print("Amicable") if num1 == sum_factors(sum_factors(num1)) and num1 != num2 else print("Not amicable")
+    if num1 == num2 or (num1 <= 0 or num2 <= 0): print("Not amicable")
+    else: print("Amicable") if sum_factors(sum_factors(num1)) == num1 and sum_factors(num1) == num2 else print("Not amicable")
     
 main()
