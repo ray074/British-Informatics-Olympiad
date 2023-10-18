923f3fec0f60629f93733c0eec565ec7c31c6216
ray074 <147860006+ray074@users.noreply.github.com>
2023-10-19 00:42:23 +0100

    Add files via upload

---
+++ Cards.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+def calc_points(cards):
+    points = 0
+    for i in range(2,6):
+        combs = combinations(cards, i)
+        if i == 2:
+            for comb in combs:
+                if comb[0] == comb[1]:
+                    points += 1
+                elif sum(comb) == 15:
+                    points += 1
+        else:
+            for comb in combs:
+                if sum(comb) == 15:
+                    points += 1
+    return points
+
+def main():
+    cards = input().split()
+    print(calc_points([int(x) for x in cards]))
+    
+main()
