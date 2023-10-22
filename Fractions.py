bafbbccb02a5eee2e7043fecb218b77876787927
ray074 <147860006+ray074@users.noreply.github.com>
2023-10-22 21:55:17 +0100

    Add files via upload

---
+++ Fractions.py
@@ -0,0 +1,11 @@
+from decimal import Decimal
+
+def convert(dec_val):
+    num, denom = dec_val.as_integer_ratio()
+    return f"{num} / {denom}"
+
+def main():
+    dec_val = Decimal(input())
+    print(convert(dec_val))
+    
+main()
