16eddbe99c5470924842cb504770617dfa92f771
Reyansh Tellis <147860006+ray074@users.noreply.github.com>
2024-01-04 12:41:25 +0000

    Update Q1: Fractions.py

--- 2005/Q1: Fractions.py
+++ 2005/Q1: Fractions.py
@@ -1,11 +1,14 @@
 from decimal import Decimal
 
+
 def convert(dec_val):
     num, denom = dec_val.as_integer_ratio()
     return f"{num} / {denom}"
 
+
 def main():
     dec_val = Decimal(input())
     print(convert(dec_val))
-    
+
+
 main()
