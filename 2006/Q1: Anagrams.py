b05c9c9d8d5ad58fa1674b120bcd9e8dcb370a69
Reyansh Tellis <147860006+ray074@users.noreply.github.com>
2024-01-13 23:43:33 +0000

    Update Q1: Anagrams.py

--- 2006/Q1: Anagrams.py
+++ 2006/Q1: Anagrams.py
@@ -1,9 +1,11 @@
 def anagrams(word1, word2):
     return sorted(list(word1)) == sorted(list(word2))
 
+
 def main():
     word1, word2 = input(), input()
     res = anagrams(word1, word2)
     print("Anagrams") if res is True else print("Not anagrams")
-    
+
+
 main()
